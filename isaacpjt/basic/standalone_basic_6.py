9d595bf4e4f3c4ddf9e1368b5bd6eb8234c53b73
suhululu <acbal0012@gmail.com>
2026-08-18 21:23:12 +0900

    0818

---
+++ isaacpjt/basic/standalone_basic_6.py
@@ -0,0 +1,37 @@
+from isaacsim import SimulationApp
+simulation_app = SimulationApp({"headless": False})     # 1. Application
+
+import numpy as np
+import time
+import omni.usd
+from isaacsim.core.api import World
+from isaacsim.core.api.objects import DynamicCuboid
+
+world = World(stage_units_in_meters=1.0)                # 2. World
+stage = omni.usd.get_context().get_stage()              # 3. Stage
+
+cube_prim1 = DynamicCuboid(                              # 4. Prim
+    prim_path="/World/redCube",
+    name="red_cube",
+    position=np.array([0.0, 0.0, 0.5]),
+    scale=np.array([0.3, 0.3, 0.3]),
+    color=np.array([1.0, 0.0, 0.0]),
+)
+
+world.scene.add_default_ground_plane()                  # 5. Scene
+world.scene.add(cube_prim1)
+
+world.reset()
+
+step_count = 0
+
+while simulation_app.is_running():
+    world.step(render=True)
+    time.sleep(0.01)
+    step_count += 1
+    if step_count % 100 == 0:
+        print(f"Step count: {step_count}")
+        if step_count >= 500:
+            break
+
+simulation_app.close()
